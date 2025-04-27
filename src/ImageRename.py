e8f1105cb728cdbfae56aa932233bf0c3fabc479
SangHyun Byun <101382267+ShByeon3968@users.noreply.github.com>
2025-04-27 23:33:41 +0900

    reset

--- src/ImageRename.py
+++
@@ -1,5 +0,0 @@
-from utils import UtillFuctions
-
-frame_numbers = [i for i in range(3,150)]  # 프레임 번호 리스트
-# Resource 디렉토리와 dirnum 변경필요
-UtillFuctions.rename_files('Resource/New/6',frame_numbers,dir_num=6)
