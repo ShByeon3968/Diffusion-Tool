0d730ef11a745389481c5c3d57d3b8a79ea762f1
SangHyun Byun <egoist12276@gmail.com>
2025-05-07 17:00:54 +0900

    Sana Installation

---
+++ cfg.py
@@ -0,0 +1,5 @@
+MODEL_NAME = {
+    "stable_diffusion_2": "stabilityai/stable-diffusion-2-1",
+    "magicILLusion": "magicILL_magicILLEPSV10.safetensors",
+
+}
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from sd_generator import StableDiffusionGenerator
+from sd_generator import DiffusionGenerator,SanaGenerator
 from background_remover import BackgroundRemover
 
-def generate_object_image_without_bg(prompt, sd_generator:StableDiffusionGenerator, bg_remover:BackgroundRemover, save_path="output.png"):
+def generate_object_image_without_bg(prompt, sd_generator:DiffusionGenerator, bg_remover:BackgroundRemover, save_path="output.png"):
     img = sd_generator.generate(prompt)
     img_nobg = bg_remover.remove_background(img)
     img_nobg.save(save_path)
@@ -9,9 +9,10 @@ def generate_object_image_without_bg(prompt, sd_generator:StableDiffusionGenerat
 
 if __name__ == "__main__":
     # 1. 초기화
-    sd_gen = StableDiffusionGenerator(model_name="stabilityai/stable-diffusion-2-1", device="cuda")
+    sd_gen = SanaGenerator()
     bg_remover = BackgroundRemover()
 
     # 2. 객체 생성 및 배경 제거
-    prompt = "a futuristic dog, isometric, green background"
-    img_nobg = generate_object_image_without_bg(prompt, sd_gen, bg_remover, save_path="robotdog.png")
+    prompt = "a cyberpunk cat with a neon sign "
+    img = sd_gen.generate(prompt)
+    img[0].save("output.png")
